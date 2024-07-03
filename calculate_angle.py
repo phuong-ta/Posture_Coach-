38e3628efd5032c68a1ac71a6759fc2d46da3768
Phuong Ta <phuongta@metropolia.fi>
2024-07-03 11:55:12 +0300

    function to calculate angle from 3 point done!

--- calculate_angle.py
+++ calculate_angle.py
@@ -2,6 +2,8 @@ import numpy as np
 
 
 # This function will calculate angle based on 3  3D coordinates
+# Set B is center point
+
 def calculate_angle(a, b, c):
     # Calculate vectors from B to A and B to C
     ba = a - b
