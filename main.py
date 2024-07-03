fc166e89b1310917d8b9af2f4286f590a585a185
Phuong Ta <phuongta@metropolia.fi>
2024-07-03 11:28:52 +0300

    main.py done!

--- main.py
+++ main.py
@@ -8,16 +8,18 @@ import numpy as np
 
 if __name__ == '__main__':
 
+    # Trainer pose
     image_processor = ImageProcessor()
     image_processor.process_images("Virabhadrasana.jpg")
     image_processor.calculate_angle()
     image_processor.display_angle_list()
 
-    
+    # trainee pose
     image_processor2 = ImageProcessor()
     image_processor2.process_images("Virabhadrasana2.jpg")
     image_processor2.calculate_angle()
     image_processor2.display_angle_list()
 
+    # compare 2 images
     if image_processor == image_processor2:
         print("Image processor match")
